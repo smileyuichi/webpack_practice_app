b68f383988db4a0f0b49770595daf3e3b46932dd
smileyuichi <smile.yuichi.1013@gmail.com>
2020-11-25 16:13:59 +0900

    optimize-css-assets-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin') 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
 
 const outputPath = path.resolve(__dirname, 'dist')
 
@@ -59,12 +60,15 @@ module.exports = {
         })
     ],
     optimization: {
-        minimizer: [new UglifyJsPlugin({
-            uglifyOptions: {
-                compress: {
-                    drop_console: true 
+        minimizer: [
+            new UglifyJsPlugin({
+                uglifyOptions: {
+                    compress: {
+                        drop_console: true 
+                    }
                 }
-            }
-        })],
+            }),
+            new OptimizeCSSAssetsWebpackPlugin({})
+        ],
     }
 }
